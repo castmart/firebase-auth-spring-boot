d771a6ebe17788b61bce9193656a121a7aedf045
JUAN CASTAÑEDA <juan.castaneda@jimdo.com>
2021-04-19 10:50:22 -0500

    First src commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # firebase-auth-spring-boot
+
 An small POC to use firebase authentication with a spring boot web app
---
+++ src/main/kotlin/com/castmart/firebaseauth/FirebaseAuthApplication.kt
@@ -0,0 +1,11 @@
+package com.castmart.firebaseauth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirebaseAuthApplication
+
+fun main(args: Array<String>) {
+    runApplication<FirebaseAuthApplication>(*args)
+}
